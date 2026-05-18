14cd3700ff43d736a38e8228bb3ff38bb7e9ef1c
cauacozz11 <cauacozz@gmail.com>
2026-05-18 22:20:09 +0000

    chore: clean up ESLint and Prisma configuration files, remove outdated comments, and delete unused test file

--- src/actions/auth/logarUsuario.test.ts
+++ src/actions/auth/logarUsuario.test.ts
@@ -5,8 +5,6 @@ import { logarUsuario } from "./logarUsuario"
 import { LoginInput } from "@/schemas/login"
 import { signIn } from "../../../auth"
 import { AuthError } from "next-auth"
-import { sign } from "crypto";
-import { ZodError } from "zod";
 
 const dadosValidos: LoginInput = {
     email: 'cauacozz@gmail.com',
